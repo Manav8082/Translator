0c05f8801dd4fcd0b63d0613a1422395ec481262
Manav Gupta <manav.gupta@cctech.co.in>
2024-10-07 19:46:46 +0530

    scxdf

--- Translator/sourcefiles/Writer.cpp
+++ Translator/sourcefiles/Writer.cpp
@@ -7,7 +7,7 @@ void write::writer(vector<triangle> get_triangles, vector<double> get_points) {
 		myfile << get_points[i.p3.x] << " " << get_points[i.p3.y] << " " << get_points[i.p3.z] << endl;
 		myfile << get_points[i.p1.x] << " " << get_points[i.p1.y] << " " << get_points[i.p1.z] << endl;
 		myfile << endl;
-		myfile << endl;
+		myfile << endl;//gfyft
 	}
 	myfile.close();
 }
