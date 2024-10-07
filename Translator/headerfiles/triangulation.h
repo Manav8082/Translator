8acf99dd406d048dc90e3c49ea3f09d994a3434f
Manav8082 <manav.gupta@cctech.co.in>
2024-10-07 19:23:52 +0530

    Delete Translator directory

--- Translator/headerfiles/triangulation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"point.h"
-#include"triangle.h"
-#include<vector>
-class triangulation {
-public:
-	vector<double> points;
-	vector<triangle> add_unique_points(vector<point> points);
-};
