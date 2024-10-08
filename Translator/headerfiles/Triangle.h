8fd6565d6e5d18d819f88f23203f1d25f2c5a2ee
Manav Gupta <manav.gupta@cctech.co.in>
2024-10-08 10:26:33 +0530

    private access specifier applied

--- Translator/headerfiles/Triangle.h
+++ Translator/headerfiles/Triangle.h
@@ -1,10 +1,11 @@
 #pragma once
 #include"point.h"
 class triangle {
-public:
+private:
 	point p1;
 	point p2;
 	point p3;
+public:
 	triangle(point p1,point p2,point p3);
 	void get_triangle();
 	~triangle();
