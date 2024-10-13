5895a7e82faff27c3116d7bb2c386fcc49e0ca8d
Manav8082 <manav.gupta@cctech.co.in>
2024-10-13 18:22:34 +0530

    Update Triangle.h

--- Translator/headerfiles/Triangle.h
+++ Translator/headerfiles/Triangle.h
@@ -1,12 +1,12 @@
 #pragma once
-#include"point.h"
-class triangle {
+#include"Point.h"
+class Triangle {
 private:
-	point p1;
-	point p2;
-	point p3;
+	Point p1;
+	Point p2;
+	Point p3;
 public:
-	triangle(point p1,point p2,point p3);
+	triangle(Point p1,Point p2,Point p3);
 	~triangle();
 	int get_p1_x_coodinate();
 	int get_p1_y_coodinate();
