25384ec9887ce21ddd0905438bc00485b9eca3a1
Manav8082 <manav.gupta@cctech.co.in>
2024-10-13 18:24:50 +0530

    Update Point.cpp

--- Translator/sourcefiles/Point.cpp
+++ Translator/sourcefiles/Point.cpp
@@ -1,10 +1,10 @@
 #include"Point.h"
-point::point(int x,int y,int z):x(x),y(y),z(z) {}
-point::~point() {}
-int point::get_x_coordinate() {
+Point::Point(int x,int y,int z):x(x),y(y),z(z) {}
+Point::~Point() {}
+int Point::get_x_coordinate() {
 	return x;
-}int point::get_y_coordinate() {
+}int Point::get_y_coordinate() {
 	return y;
-}int point::get_z_coordinate() {
+}int Point::get_z_coordinate() {
 	return z;
 }
