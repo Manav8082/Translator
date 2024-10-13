257d68edd633c3a427d7891fb8ffd93b35095de5
Manav8082 <manav.gupta@cctech.co.in>
2024-10-13 18:28:58 +0530

    Update Triangulation.cpp

--- Translator/sourcefiles/Triangulation.cpp
+++ Translator/sourcefiles/Triangulation.cpp
@@ -3,16 +3,16 @@
 #include"Reader.h"
 #include"Triangle.h"
 #include<vector>
-vector<triangle> triangulation::add_unique_points(vector<point> points){
+vector<triangle> Triangulation::add_unique_points(vector<Point> points){
 	
-	vector<triangle> get_triangle;
+	vector<Triangle> get_triangle;
 	if (points.size() % 3 == 0) {
 		int differentiate_triangle = points.size() / 3;
 		for (int i = 0; i < differentiate_triangle; i++) {
-			point p1(points[(i * 3) + 0].get_x_coordinate(), points[(i * 3) + 0].get_y_coordinate(), points[(i * 3) + 0].get_z_coordinate());
-			point p2(points[(i * 3) + 1].get_x_coordinate(), points[(i * 3) + 1].get_y_coordinate(), points[(i * 3) + 1].get_z_coordinate());
-			point p3(points[(i * 3) + 2].get_x_coordinate(), points[(i * 3) + 2].get_y_coordinate(), points[(i * 3) + 2].get_z_coordinate());
-			triangle t1(p1, p2, p3);
+			Point p1(points[(i * 3) + 0].get_x_coordinate(), points[(i * 3) + 0].get_y_coordinate(), points[(i * 3) + 0].get_z_coordinate());
+			Point p2(points[(i * 3) + 1].get_x_coordinate(), points[(i * 3) + 1].get_y_coordinate(), points[(i * 3) + 1].get_z_coordinate());
+			Point p3(points[(i * 3) + 2].get_x_coordinate(), points[(i * 3) + 2].get_y_coordinate(), points[(i * 3) + 2].get_z_coordinate());
+			Triangle t1(p1, p2, p3);
 			get_triangle.push_back(t1);
 		}
 	}
