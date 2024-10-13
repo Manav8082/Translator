d3396683fb1b58454d67cb9420339c53e76d50dc
Manav8082 <manav.gupta@cctech.co.in>
2024-10-13 18:31:21 +0530

    Update Translator.cpp

--- Translator/Translator.cpp
+++ Translator/Translator.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#include"triangulation.h"
-#include"reader.h"
+#include"Triangulation.h"
+#include"Reader.h"
 #include"Writer.h"
 using namespace std;
 int main() {
-	read re;
-	vector<point> get_points = re.reader();
+	Read re;
+	vector<Point> get_points = re.reader();
 	vector<double> u_points = re.unique();
-	triangulation t;
-	vector<triangle> get_triangles = t.add_unique_points( get_points);
-	write w;
+	Triangulation t;
+	vector<Triangle> get_triangles = t.add_unique_points( get_points);
+	Write w;
 	w.writer(get_triangles,u_points);
 
 
